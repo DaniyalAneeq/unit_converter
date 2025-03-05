c98b1b89597fcd84361994e600f1f96d9db3b8a3
DANIAL ANEEQ AHMED <167881750+DaniyalAneeq@users.noreply.github.com>
2025-03-05 06:23:34 +0500

    update UI

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 st.title('🧮 Unit Converter')
 st.markdown("### Converts Length, Weight, Temperature and Time units")
-st.write("Welcome to the Unit Converter! Select the type of conversion you'd like to perform")
+st.info("Welcome to the Unit Converter! Select the type of conversion you'd like to perform")
 
 category = st.selectbox("Select a category", ["Length", "Weight", "Time", "Temperature"])
 
